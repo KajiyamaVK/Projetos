b212f8277549fabb175aafea09ecc25f8353ce3b
Victor Kajiyama <Victor Kajiyama>
2020-11-06 11:35:59 -0300

    x

---
+++ Fidelity/Fidelity/Fidelity/Controllers/ClientesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fidelity.Controllers
+{
+    [Route("clientes")]
+    [ApiController]
+    public class ClientesController : Controller
+    {
+        public List<Clientes> Index()
+        {
+            return View();
+        }
+    }
+}
